d8b847c5a1bb84eebde42e81bde6ecee20fc6632
joshuaunity <oghenerobojosh01@gmail.com>
2024-10-14 18:45:00 +0100

    chore: update code

--- models/User.js
+++ models/User.js
@@ -1,34 +1,38 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../db');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../db");
 
-const User = sequelize.define('User', {
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true,
+const User = sequelize.define(
+    "User",
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
         },
     },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-}, {
-    timestamps: true,
-});
+    {
+        timestamps: true,
+    }
+);
 
 User.associate = (models) => {
-    User.hasMany(models.Expense, {foreignKey: 'UserId', as: 'expenses'});
+    User.hasMany(models.Expense, { foreignKey: "UserId", as: "expenses" });
 };
 
 module.exports = User;
